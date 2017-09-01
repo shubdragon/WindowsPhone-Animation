4d1d99a79bd8ac03ca3d28782f8000ffaa34e049
shubdragon <shubham.sahu@hotmail.com>
2017-09-01 12:43:23 +0530

    Circular anmiation

---
+++ Anmiation/Anmiation/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Anmiation.Resources;
+using System.Threading.Tasks;
+
+namespace Anmiation
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += async (s, e) =>
+            {
+                await Task.Delay(500);
+                CircularWarningAnimation1.Begin();
+            };
+        }        
+    }
+}
